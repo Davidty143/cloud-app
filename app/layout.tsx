97212eb931e3c83f4008e9a5c971fb1ee165c397
DavidTy <davidantonio.ty6@gmail.com>
2025-02-19 21:24:21 +0800

    Layout done

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,119 +23,127 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("bg-background", inter.className)}>
-        {/* Menu Header */}
-        <div
-          className={cn(
-            "menu-header",
-            "bg-blue-200",
-            "h-16",
-            "w-full",
-            "flex",
-            "items-center",
-            "justify-center"
-          )}
-        >
-          {/* Inner Div centered within the Menu */}
-          <div
-            className={cn(
-              "menu-header",
-              "bg-blue-500",
-              "h-full", // Use full height for inner div
-              "w-[1250px]",
-              "2xl:w-[80%]",
-              "flex",
-              "justify-between", // Space out the children in the flex container
-              "items-center" // Center the items vertically
-            )}
-          >
-            {/* LOGO Section */}
+        {/* Wrapper div to hold all elements */}
+        <div className="w-full">
+          {/* Wrapper for both headers */}
+          <div className="header-wrapper">
+            {/* Menu Header */}
             <div
               className={cn(
                 "menu-header",
-                "bg-red-300",
-                "h-full", // Take full height of the parent
-                "w-[150px]", // Width as 10% of parent
+                "bg-blue-200",
+                "h-16",
+                "w-full",
                 "flex",
                 "items-center",
                 "justify-center"
               )}
             >
-              LOGO
+              {/* Inner Div centered within the Menu */}
+              <div
+                className={cn(
+                  "menu-header",
+                  "bg-blue-500",
+                  "h-full", // Use full height for inner div
+                  "w-[1250px]",
+                  "2xl:w-[80%]",
+                  "flex",
+                  "justify-between", // Space out the children in the flex container
+                  "items-center" // Center the items vertically
+                )}
+              >
+                {/* LOGO Section */}
+                <div
+                  className={cn(
+                    "menu-header",
+                    "bg-red-300",
+                    "h-full", // Take full height of the parent
+                    "w-[150px]", // Width as 10% of parent
+                    "flex",
+                    "items-center",
+                    "justify-center"
+                  )}
+                >
+                  LOGO
+                </div>
+
+                {/* Login Section */}
+                <div
+                  className={cn(
+                    "menu-header",
+                    "bg-violet-300",
+                    "h-full", // Take full height of the parent
+                    "w-[300px]", // Width as 10% of parent
+                    "flex",
+                    "items-center",
+                    "justify-end",
+                    "flex-shrink-0"
+                  )}
+                >
+                  <SignupButton />
+                  <LoginButton />
+                </div>
+              </div>
             </div>
 
-            {/* Login Section */}
+            {/* Bottom Green Header */}
             <div
               className={cn(
                 "menu-header",
-                "bg-violet-300",
-                "h-full", // Take full height of the parent
-                "w-[300px]", // Width as 10% of parent
+                "bg-green-200",
+                "h-16",
+                "w-full",
                 "flex",
                 "items-center",
-                "justify-end"
+                "justify-center"
               )}
             >
-              <SignupButton />
-              <LoginButton />
+              <div
+                className={cn(
+                  "menu-header",
+                  "bg-yellow-200",
+                  "h-full", // Use full height for inner div
+                  "w-[1250px]",
+                  "2xl:w-[80%]",
+                  "flex",
+                  "justify-between", // Space out the children in the flex container
+                  "items-center" // Center the items vertically
+                )}
+              >
+                {" "}
+                <div
+                  className={cn(
+                    "menu-header",
+                    "bg-pink-300",
+                    "h-full", // Take full height of the parent
+                    "w-[200px]", // Width as 10% of parent
+                    "flex",
+                    "items-center",
+                    "justify-center"
+                  )}
+                >
+                  <Header />
+                </div>
+                <div
+                  className={cn(
+                    "menu-header",
+                    "bg-orange-300",
+                    "h-full", // Take full height of the parent
+                    "w-[300px]", // Width as 10% of parent
+                    "flex",
+                    "items-center",
+                    "justify-center"
+                  )}
+                >
+                  <SearchBar />
+                </div>
+              </div>
             </div>
-          </div>
+          </div>{" "}
+          {/* End of header-wrapper */}
+          {/* Children content */}
+          {children}
         </div>
-
-        {/* Bottom Green Header */}
-        <div
-          className={cn(
-            "menu-header",
-            "bg-green-200",
-            "h-16",
-            "w-full",
-            "flex",
-            "items-center",
-            "justify-center"
-          )}
-        >
-          <div
-            className={cn(
-              "menu-header",
-              "bg-yellow-200",
-              "h-full", // Use full height for inner div
-              "w-[1250px]",
-              "2xl:w-[80%]",
-              "flex",
-              "justify-between", // Space out the children in the flex container
-              "items-center" // Center the items vertically
-            )}
-          >
-            {" "}
-            <div
-              className={cn(
-                "menu-header",
-                "bg-pink-300",
-                "h-full", // Take full height of the parent
-                "w-[200px]", // Width as 10% of parent
-                "flex",
-                "items-center",
-                "justify-center"
-              )}
-            >
-              <Header />
-            </div>
-            <div
-              className={cn(
-                "menu-header",
-                "bg-orange-300",
-                "h-full", // Take full height of the parent
-                "w-[300px]", // Width as 10% of parent
-                "flex",
-                "items-center",
-                "justify-center"
-              )}
-            >
-              <SearchBar />
-            </div>
-          </div>
-        </div>
-
-        {children}
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-red-200 min-h-screen flex justify-center">
-      <div className="bg-green-200 w-[1250px] 2xl:w-[80%]"></div>
+    <main>
+      {/* Main Wrapper */}
+      <div className="bg-red-200 min-h-screen flex justify-center">
+        {/* Focus Content */}
+        <div className="bg-green-200 w-[1250px] 2xl:w-[80%] flex flex-col">
+          {/* Main Body */}
+          <div className="bg-purple-200 w-full h-[80vh] flex flex-row justify-between">
+            {/* Content Body */}
+            <div className="bg-blue-200 w-full">CONTENT</div>
+            {/* Sidebar Body */}
+            <div className="bg-gray-200 w-[300px] flex-shrink-0">
+              SIDE PANEL
+            </div>
+          </div>
+          {/* Footer */}
+          <div className="bg-orange-200 w-full h-[20vh]">FOOTER</div>
+        </div>
+      </div>
     </main>
   );
 }
